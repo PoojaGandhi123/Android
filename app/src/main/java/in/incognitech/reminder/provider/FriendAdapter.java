19daae603d61a3910289c60360ab521d7150f0b2
Udit Desai <desaiuditd@gmail.com>
2016-02-20 13:10:44 -0800

    fixes #31 - filter friend list

--- app/src/main/java/in/incognitech/reminder/provider/FriendAdapter.java
+++ app/src/main/java/in/incognitech/reminder/provider/FriendAdapter.java
@@ -1,18 +1,16 @@
 package in.incognitech.reminder.provider;
 
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 import in.incognitech.reminder.R;
 import in.incognitech.reminder.model.Friend;
@@ -20,26 +18,29 @@ import in.incognitech.reminder.model.Friend;
 /**
  * Created by udit on 17/02/16.
  */
-public class FriendAdapter extends ArrayAdapter<Friend> {
+public class FriendAdapter extends ArrayAdapter<Friend> implements Filterable {
 
     private List<Friend> friendList;
-    private String phoneNumber;
-    private String message;
+    private List<Friend> filteredFriendList;
+    private FriendFilter friendFilter = new FriendFilter();
+
     static class ViewHolder {
         TextView name;
         TextView email;
-        Button sendInvites;
     }
 
     public FriendAdapter(Context context, int resource, List<Friend> friendList) {
         super(context, resource, friendList);
-        this.friendList = friendList;
+        this.friendList = new ArrayList<Friend>();
+        this.friendList.addAll(friendList);
+        this.filteredFriendList = new ArrayList<Friend>();
+        this.filteredFriendList.addAll(friendList);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        Friend friend = this.getItem(position);
+        Friend friend = filteredFriendList.get(position);
 
         View row;
         ViewHolder holder;
@@ -50,27 +51,6 @@ public class FriendAdapter extends ArrayAdapter<Friend> {
             holder = new ViewHolder();
             holder.name = (TextView) row.findViewById(R.id.friend_display_name);
             holder.email = (TextView) row.findViewById(R.id.friend_email);
-            holder.sendInvites=(Button)row.findViewById(R.id.inviteFriends);
-            holder.sendInvites.setVisibility(View.VISIBLE);
-            holder.sendInvites.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    Toast.makeText(getContext(),"Button Clicked",Toast.LENGTH_SHORT).show();
-
-                    phoneNumber="191";
-                    message="Testing Out Loud";
-                    Intent smsIntent = new Intent(Intent.ACTION_VIEW);
-
-                    smsIntent.setData(Uri.parse("smsto:"));
-                    smsIntent.setType("vnd.android-dir/mms-sms");
-                    smsIntent.putExtra("address"  , phoneNumber);
-                    smsIntent.putExtra("sms_body", message);
-
-
-
-                }
-            });
             row.setTag(holder);
         } else {
             row = convertView;
@@ -83,19 +63,53 @@ public class FriendAdapter extends ArrayAdapter<Friend> {
         return row;
     }
 
-    public void filter(String keyword) {
-        keyword = keyword.toLowerCase(Locale.getDefault());
-        this.clear();
-        if (keyword.length() == 0) {
-            this.addAll(friendList);
-        } else {
-            for (Friend f : friendList) {
-                if (f.getName().toLowerCase(Locale.getDefault())
-                        .contains(keyword)) {
-                    this.add(f);
+    @Override
+    public Filter getFilter() {
+        return friendFilter;
+    }
+
+    private class FriendFilter extends Filter {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            String keyword = constraint.toString().toLowerCase();
+            FilterResults results = new FilterResults();
+
+            if(keyword != null && keyword.toString().length() > 0) {
+
+                ArrayList<Friend> filteredItems = new ArrayList<Friend>();
+                String email, name;
+
+                for (int i = 0, l = friendList.size(); i < l; i++) {
+                    name = friendList.get(i).getName();
+                    email = friendList.get(i).getEmail();
+                    if (name.toLowerCase().contains(keyword) || email.toLowerCase().contains(keyword)) {
+                        filteredItems.add(friendList.get(i));
+                    }
+                }
+                results.count = filteredItems.size();
+                results.values = filteredItems;
+            } else {
+                synchronized(this)
+                {
+                    results.values = friendList;
+                    results.count = friendList.size();
                 }
             }
+
+            return results;
         }
-        notifyDataSetChanged();
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            filteredFriendList = (ArrayList<Friend>)results.values;
+//            notifyDataSetChanged();
+            clear();
+            for(int i = 0, l = filteredFriendList.size(); i < l; i++) {
+                add(filteredFriendList.get(i));
+            }
+//            notifyDataSetInvalidated();
+            notifyDataSetChanged();
+        }
     }
 }
