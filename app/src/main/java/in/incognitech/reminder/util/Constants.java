cb33b65b19481fb97149f03347ac9e37f6653f88
Udit Desai <desaiuditd@gmail.com>
2016-03-16 09:48:42 -0700

    fixes date format for 24 hours

--- app/src/main/java/in/incognitech/reminder/util/Constants.java
+++ app/src/main/java/in/incognitech/reminder/util/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 
     public static final String IMAGE_CACHE_DIR = "thumbs";
 
-    public static final String DATE_FORMAT = "dd-MM-yyyy hh:mm:ss";
+    public static final String DATE_FORMAT = "dd-MM-yyyy HH:mm:ss";
 
     public static String SHARED_PREFS_KEY = "reminder-shared-prefs";
     public static String SHARED_PREFS_PROCESSED_CONTACTS = "processed-contacts";
